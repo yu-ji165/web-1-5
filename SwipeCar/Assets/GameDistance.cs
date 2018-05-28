cfd1c262c27f3c8df10073e95351badf752b8b13
yu-ji <enpatsushakugan@icloud.com>
2018-05-28 17:51:21 +0900

    変数名若干省略しました

---
+++ SwipeCar/Assets/GameDistance.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDistance : MonoBehaviour
+{
+
+    GameObject car;
+    GameObject flag;
+    GameObject dis;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.dis = GameObject.Find("Distance");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float len = this.flag.transform.position.x - this.car.transform.position.x;
+        this.dis.GetComponent<Text>().text = "ゴールまで" + len.ToString("F2") + "m";
+    }
+}
